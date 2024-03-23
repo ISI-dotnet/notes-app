f65fe96e6bd57e41c43e5e863e8705c20e98d072
Ech0ke <aidas.zygas@gmail.com>
2024-03-23 10:48:30 +0200

    Add env variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,24 @@
-import { getAuth } from '@firebase/auth';
-import { initializeApp } from '@firebase/app';
-import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "@firebase/auth"
+import { initializeApp } from "@firebase/app"
+import { getFirestore } from "@firebase/firestore"
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env"
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyC_A0Ov-pIP4wN2tuq3WuEwR4fvYLADcuE',
-    authDomain: 'vu-noteapp-v2.firebaseapp.com',
-    databaseURL: 'https://vu-noteapp-v2-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'vu-noteapp-v2',
-    storageBucket: 'vu-noteapp-v2.appspot.com',
-    messagingSenderId: '683996676974',
-    appId: '1:683996676974:android:e3e50105f4fccae6a3777f',
-    measurementId: 'G-measurement-id',
-  };
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+}
 
-
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
-export const auth = getAuth(app);
+export const auth = getAuth(app)
